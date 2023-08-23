b60d82e60c80e658470a7d62ca8718a749ee65d2
Horbunov Yurii <urijnet@gmail.com>
2023-08-23 16:17:45 +0300

    tasck-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Form } from './Form/Form';
+import { ContactsList } from './ContactsList/ContactsList';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+    number: '',
+  };
+
+  hendleSubmit = newContact => {
+    const isExist = this.state.contacts.find(
+      ({ number }) => number === newContact.number
+    );
+    if(isExist){
+      alert('contact already exists')
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  hendleDelitContact = idContact => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== idContact),
+    }));
+  };
+
+  render() {
+    const {contacts} = this.state;
+    return (
+      <>
+        <Form onSubmit={this.hendleSubmit} />
+        <h3>Contacts</h3>
+        <ContactsList contacts = {contacts} deleteContacts = {this.hendleDelitContact}/>
+     
+      </>
+    );
+  }
+}
